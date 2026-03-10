ca4136f6ebc26b4a32ff11a2bdcb6004eef1813b
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-03-10 11:17:13 +0700

    add

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,6 @@
 public class Client {
     public void share(String mess) {
         MediaFacade mediaFacade = new MediaFacade(new Facebook(), new Twitter(), new LinkedIn());
-        mediaFacade.MediaShare("abc");
+        mediaFacade.MediaShare(mess);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Client client = new Client();
-        client.share("This is a post");
+        client.share("than dong Huu");
     }
 }
